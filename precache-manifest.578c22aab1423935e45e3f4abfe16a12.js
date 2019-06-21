0fcb9a2e6b5a72a53037d0219bb4799f0645018f
LNorio <leonardocruz@alunos.utfpr.edu.br>
2019-06-21 16:40:11 -0300

    Updates

---
+++ precache-manifest.578c22aab1423935e45e3f4abfe16a12.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "834ff3247bc4330836ebda0c43a97d18",
+    "url": "/portifolio/index.html"
+  },
+  {
+    "revision": "49dfc9c3ab099ca9f8b8",
+    "url": "/portifolio/static/css/main.97de4345.chunk.css"
+  },
+  {
+    "revision": "c76acbd8c3450c465efd",
+    "url": "/portifolio/static/js/2.4cc8969d.chunk.js"
+  },
+  {
+    "revision": "49dfc9c3ab099ca9f8b8",
+    "url": "/portifolio/static/js/main.f807467c.chunk.js"
+  },
+  {
+    "revision": "91a74ff0904045e04265",
+    "url": "/portifolio/static/js/runtime~main.e9cc7322.js"
+  },
+  {
+    "revision": "f0d85339d77566fe1103b17f3978711a",
+    "url": "/portifolio/static/media/Curriculo.f0d85339.pdf"
+  },
+  {
+    "revision": "48b962d5b079cd189e9c7a6b6dc87b77",
+    "url": "/portifolio/static/media/E3-2019.48b962d5.jpg"
+  },
+  {
+    "revision": "8f1a518ff69f09062a1e6f1df181ee52",
+    "url": "/portifolio/static/media/Geovanna.8f1a518f.jpg"
+  },
+  {
+    "revision": "a062e31f93745c5302747a81797b4e84",
+    "url": "/portifolio/static/media/Google.a062e31f.jpg"
+  },
+  {
+    "revision": "e68bb722b9638cb3fc9f62075d4c4750",
+    "url": "/portifolio/static/media/MyPhoto.e68bb722.jpg"
+  },
+  {
+    "revision": "ef436697e27c6d767b2e739342ddc690",
+    "url": "/portifolio/static/media/SODM.ef436697.jpg"
+  },
+  {
+    "revision": "fcdb090f04580204247031b0ba155c74",
+    "url": "/portifolio/static/media/Scarlett.fcdb090f.jpg"
+  },
+  {
+    "revision": "64e7b57db9b5077812a58fe3a73df358",
+    "url": "/portifolio/static/media/SkillBack.64e7b57d.jpg"
+  }
+]);
